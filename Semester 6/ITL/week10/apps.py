f47f1bb48bdf4a106f1360c5eeb80cb2e4b40e27
Sweta <93282010+Sweta6420@users.noreply.github.com>
2024-03-12 11:10:39 +0530

    Add files via upload

---
+++ Semester 6/ITL/week10/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class lab10Config(AppConfig):
+    name = 'lab10'
---
+++ Semester 6/ITL/week10/urls.py
@@ -0,0 +1,13 @@
+
+from django.conf.urls import include, url
+from django.urls import path
+from . import views
+urlpatterns = [   
+    path('archive/', views.archive, name="archive"),
+    path('archive/createblog/', views.create_blog, name="createblog"),
+    path('', views.index, name='index'),
+    path('createpage/', views.create, name='create'),
+    path('home/add/', views.add, name='add'),
+    path('home/get/', views.get, name='filter'),
+    path('home/',views.home,name='home'),
+]  
